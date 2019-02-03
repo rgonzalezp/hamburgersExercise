96a95ae45246b50a95d0434deead855ac2dfb9d3
Federico Gonzalez <rgonzalezp1115@gmail.com>
2019-02-03 14:26:42 -0500

    Initial Commit

---
+++ moreHamburgers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moreHamburgers
+//
+//  Created by Federico Gonzalez on 2/3/19.
+//  Copyright © 2019 Ricardo Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
